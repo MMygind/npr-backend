590fd2c53dd527b55f74cd1c97b35c9ae26d96fc
MMygind <56871472+MMygind@users.noreply.github.com>
2021-11-10 12:55:12 +0100

    GetAllTransactions Method

--- src/api/controllers/transactions/transactions.controller.ts
+++ src/api/controllers/transactions/transactions.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { TransactionService } from '../../../core/services/transaction/transaction.service';
+import { ApiResponse } from '@nestjs/swagger';
 
 @Controller('transactions')
-export class TransactionsController {}
+export class TransactionsController {
+  constructor(private service: TransactionService) {}
+
+  @Get()
+  @ApiResponse({ status: 200, description: 'Gets all transactions' })
+  async getAllTransactions() {
+    return await this.service.getAllTransactions();
+  }
+}
--- src/core/services/transaction/transaction.service.ts
+++ src/core/services/transaction/transaction.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { TransactionEntity } from '../../../infrastructure/entities/transaction.entity';
 import { Repository } from 'typeorm';
+import { TransactionModel } from '../../models/transaction.model';
 
 @Injectable()
 export class TransactionService {
@@ -9,4 +10,14 @@ export class TransactionService {
     @InjectRepository(TransactionEntity)
     private transactionRepository: Repository<TransactionEntity>,
   ) {}
+
+  async getAllTransactions(): Promise<TransactionModel[]> {
+    const transactions = await this.transactionRepository.find({
+      relations: ['washtype', 'location', 'licenseplate'],
+    });
+    const transactionEntities: TransactionModel[] = JSON.parse(
+      JSON.stringify(transactions),
+    );
+    return transactionEntities;
+  }
 }
