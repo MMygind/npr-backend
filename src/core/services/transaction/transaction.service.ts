96e5016d0295e2e8311944d5a5bbafc97878acb7
MMygind <56871472+MMygind@users.noreply.github.com>
2021-11-16 11:44:37 +0100

    Order by

--- src/core/services/transaction/transaction.service.ts
+++ src/core/services/transaction/transaction.service.ts
@@ -31,6 +31,9 @@ export class TransactionService {
         'licensePlate.customer',
         'licensePlate.customer.subscription',
       ],
+      order: {
+        id: 'ASC',
+      },
     });
   }
 
