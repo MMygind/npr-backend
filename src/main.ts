b1caa057236dea3493bb2d7f2bb2a72e03176b9e
MMygind <56871472+MMygind@users.noreply.github.com>
2021-12-06 14:18:08 +0100

    deployment v2

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT || 3000);
+  const configService: ConfigService = app.get(ConfigService);
+  await app.listen(configService.get('PORT') || 3000);
 }
 bootstrap();
