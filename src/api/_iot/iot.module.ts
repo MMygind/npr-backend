612acc106b169ae1ea4c8a0df4eb19892e6fbbd7
MMygind <56871472+MMygind@users.noreply.github.com>
2022-01-04 11:21:12 +0100

    Refactorv2

--- src/api/_iot/iot.module.ts
+++ src/api/_iot/iot.module.ts
@@ -3,9 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TransactionService } from '../../core/services/transaction/transaction.service';
 import { TransactionEntity } from '../../infrastructure/entities/transaction.entity';
 import { IotController } from './controllers/iot.controller';
+import { CustomerModule } from '../_web/controllers/customer.module';
+import { LocationModule } from '../_web/controllers/location.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TransactionEntity])],
+  imports: [TypeOrmModule.forFeature([TransactionEntity]),
+    CustomerModule,
+    LocationModule],
   controllers: [IotController],
   providers: [TransactionService],
 })
